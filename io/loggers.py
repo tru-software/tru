6bda3598d675e444b9a90a7536f09a6596e7ebd8
Tomasz Hławiczka <tomaszhlawiczka@users.noreply.github.com>
2017-11-10 12:42:47 +0100

    Logging workaround

--- io/loggers.py
+++ io/loggers.py
@@ -5,15 +5,23 @@ class UTF8Formatter(logging.Formatter):
 
 	def format(self, *args, **kwargs):
 		r = super(UTF8Formatter, self).format(*args, **kwargs)
-		if isinstance(r, str):
-			r = r.encode('utf8')
+		#if isinstance(r, str):
+			#r = r.encode('utf8')
 		return r
 
 	def formatTime(self, *args, **kwargs):
 		r = super(UTF8Formatter, self).formatTime(*args, **kwargs)
-		if isinstance(r, str):
-			r = r.encode('utf8')
+		#if isinstance(r, str):
+			#r = r.encode('utf8')
 		return r
 
+class SysLogHandler(logging.handlers.SysLogHandler):
 
-# logging.UTF8Formatter = UTF8Formatter
+	def format(self, record):
+		msg = super(SysLogHandler, self).format(record)
+		if isinstance(msg, bytes):
+			return msg.decode('utf8')
+		return msg
+
+logging.handlers.SysLogHandler = SysLogHandler
+#logging.UTF8Formatter = UTF8Formatter
