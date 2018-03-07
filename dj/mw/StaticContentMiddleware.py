bc1d3fc0c53ea045b56f610a20538f00dfc1a957
kamilhlawiczka <kamil.hlawiczka@gmail.com>
2018-03-07 12:37:27 +0100

    Upgrade PY [2.7 → 3.6] and Djanog [1.11 → 2.0]

--- dj/mw/StaticContentMiddleware.py
+++ dj/mw/StaticContentMiddleware.py
@@ -3,15 +3,14 @@
 import re
 
 import settings
-from django.core import urlresolvers
+from django.urls.resolvers import URLResolver, RegexPattern
 from django.urls import Resolver404
 
 def StaticContentMiddleware(get_response):
 
 	def process_request(request):
 
-		resolver = urlresolvers.RegexURLResolver(r'^/', settings.ROOT_URLCONF)
-
+		resolver = URLResolver(RegexPattern(r'^/'), settings.ROOT_URLCONF)
 		try:
 			callback, callback_args, callback_kwargs = resolver.resolve(request.path)
 			return callback(request, *callback_args, **callback_kwargs)
