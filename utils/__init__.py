668ae342231a5dff1b9934066288b85e25b07adf
kamilhlawiczka <kamil.hlawiczka@gmail.com>
2018-05-29 15:27:54 +0200

    Parse page number

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from .backtrace import *
+from .pagging import *
---
+++ utils/pagging.py
@@ -0,0 +1,9 @@
+def parse_page_number(string, maxv=None, minv=1):
+	if string.isdigit():
+		page = int(string)
+		if maxv:
+			return min(maxv, max(minv, page))
+		else:
+			return max(minv, page)
+	else:
+		return minv
