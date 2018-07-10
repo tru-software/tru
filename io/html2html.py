eb27c112b43cfb97c2230c93a6b8aadf7bde62cd
Tomasz Hławiczka <tomasz.hlawiczka@tru.com>
2018-07-10 22:18:01 +0200

    service theme descriptor

---
+++ io/html2html.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+# import tidy
+
+from bs4 import BeautifulSoup, Tag, NavigableString, Comment, Declaration, ProcessingInstruction, CData
+
+# ------------------------------------------------------------------------
+
+class Filter:
+	
+	parse_unknown = False
+
+	def check(self, tag):
+		return self.tag(tag, tag.attrs)
+
+	def tag(self, tag, attr):
+		if tag.name not in self.tags:
+			if self.parse_unknown is True:
+				return None
+
+			return False
+
+		d = self.tags[tag.name]
+
+		for a, k in attr.items():
+			try:
+				if (a in d) is False:
+					del tag[a]
+				elif d[a] is not None and d[a](k) is False:
+					del tag[a]
+			except AttributeError:
+				del tag[a]
+
+		return True
+
+	def parse_style(self, style):
+		css = []
+		for c in style.split(';'):
+			v = c.split(':')
+			if len(v) != 2:
+				continue
+			css.append( ( v[0].strip(), v[1].strip() ) )
+		return css
+
+	def clean(self, soup):
+		for e in soup.contents[:]:
+			self.clean_part(e)
+
+	def clean_part(self, node):
+		if isinstance(node,Comment) or isinstance(node,Declaration) or isinstance(node,ProcessingInstruction) or isinstance(node,CData):
+			node.extract()
+		elif isinstance(node, NavigableString):
+			pass
+		elif isinstance(node,Tag):
+
+			x = self.check( node )
+			if x == False:
+				node.extract()
+			else:
+				for e in node.contents[:]:
+					self.clean_part(e)
+
+				if x == None:
+					i = node.parent.contents.index(node)+1
+					for e in node.contents[:]:
+						node.parent.insert(i, e)
+					node.extract()
+
+# ------------------------------------------------------------------------
+
+class BasicFilter(Filter):
+
+	def __init__(self):
+		self.tags = {'strong':None, 'b':None, 'u':None, 'i':None, 'em':None, 'p':None}
+
+# ------------------------------------------------------------------------
+
+class PagerFilter(Filter):
+	def __init__(self):
+		self.tags = {}
+		self.pages = []
+	
+	def clean(self, soup):
+		self.pages.append('')
+		for e in soup.contents:
+			if isinstance(e,Tag) and e.name == 'div' and e._getAttrMap().get('style',None) == 'page-break-after: always;':
+				self.pages.append('')
+			else:
+				self.pages[-1] += e.__str__()
+
+# ------------------------------------------------------------------------
+
+class CommentFilter(Filter):
+	def __init__(self):
+		self.tags = {
+			'a':{'href':None},
+			'b':None,
+			'strong':None,
+			'u':None,
+			'i':None,
+			'em':None,
+			'p':None,
+			'blockquote':None,
+		}
+
+# ------------------------------------------------------------------------
+
+class BlogFilter(Filter):
+
+	def __init__(self):
+		# wszystkie znaczniki z edytorka html
+		self.tags = {
+			'a':{'href':None},
+			's':{'class':None},
+			'b':{'class':None},
+			'ol':{'class':None},
+			'ul':{'class':None},
+			'table':None,
+			'h1':{'class':None},
+			'h2':{'class':None},
+			'h3':{'class':None},
+			'h4':{'class':None},
+			'h5':{'class':None},
+			'h6':{'class':None},
+			'td':{'class':None},
+			'tr':{'class':None},
+			'address':{'class':None},
+			'strong':{'class':None},
+			'q':{'class':None},
+			'cite':{'class':None},
+			'kbd':{'class':None},
+			'var':{'class':None},
+			'ins':{'class':None},
+			'del':{'class':None},
+			'samp':{'class':None},
+			'pre':{'class':None},
+			'div':{'class':None},
+			'code':{'class':None},
+			'li':{'class':None},
+			'br':None,
+			'strong':{'class':None},
+			'u':None,
+			'i':None,
+			'em':None,
+			'p':{'class':None, 'style':None},
+			'blockquote':None,
+			'img':{
+				'src':None,
+				'width':lambda v: v.isdigit() and int(v) >= 2 and int(v) < 530,
+				'height':lambda v: v.isdigit() and int(v) >= 2 and int(v) < 530,
+				'alt':None
+			},
+			'span':{'style':self._span_styles,'class':None}
+		}
+	
+	@staticmethod
+	def _is_color(value):
+		return re.match( r'^rgb *\( *[0-9]{1,3} *, *[0-9]{1,3} *, *[0-9]{1,3} *\)$', value) != None or \
+			re.match( r'^#[0-9a-fA-F]{3}$', value) != None or \
+			re.match( r'^#[0-9a-fA-F]{6}$', value) != None
+	
+	def _span_styles(self, style):
+		css = self.parse_style(style)
+		
+		for n,v in css:
+			# print "style: '%s'='%s'" % (n,v)
+			if n == 'font-size':
+				if not v in ('xx-large', 'x-large', 'large', 'medium', 'small', 'x-small', 'xx-small', 'larger', 'smaller'):
+					return False
+			elif n == 'color':
+				if self._is_color(v) != True:
+					return False
+			else:
+				return False
+		return True
+
+# ------------------------------------------------------------------------
+
+class HTMLCleaner:
+
+	def parse( self, code, filter = None ):
+
+		if filter is None:
+			filter = BasicFilter()
+
+		soup = BeautifulSoup(code, "html.parser")
+		filter.clean( soup )
+		# return soup.prettify(encoding=None);
+		return soup.__str__()
+
+# ------------------------------------------------------------------------
+
+class HTMLPager:
+
+	def parse( self, code ):
+
+		filter = PagerFilter()
+		soup = BeautifulSoup(code, "html.parser")
+		filter.clean(soup)
+		return filter.pages
+
+if __name__ == "__main__":
+	c = HTMLCleaner()
+#	print c.parse( '<a href="xx"> dupa <b> dupa </b> dupa </a> <img src="dupa" width="2"/>', BlogFilter() )
+#	print c.parse( '<span style="font-size:small; font-size : larger ">XXX</span>', BlogFilter() )
+#	print c.parse( '<p><span style="color: rgb(51, 51, 0);color:#A00000;">asdasdasd</span></p>', BlogFilter() )
+#	print c.parse( '<b>A<h1>B</h1>C</b>', BasicFilter() )
+
+# ------------------------------------------------------------------------
+
+def cleanup_xhtml(content, strip_comments=False):
+
+	soup = BeautifulSoup(content, 'lxml')
+	comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+	[comment.extract() for comment in comments]
+	return str(soup)
+
+	content = content.encode('utf8') if isinstance(content, str) else content
+
+	# http://tidy.sourceforge.net/docs/quickref.html
+	return str(tidy.parseString(content,
+		output_xhtml=1,
+		wrap=0,
+		hide_comments=strip_comments,
+		clean=0,
+		indent=0,
+		tidy_mark=0,
+		quote_marks=1,
+		show_body_only='auto',
+		output_encoding='utf8',
+		input_encoding='utf8'
+	))
---
+++ lib/Themes/ServiceTheme.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from django.conf import settings
+import logging
+import os
+import imp
+import zlib
+import json
+
+from tru.fs import stamp as filestamp
+from tru.dj.responses import FileInMemory
+from tru.io.html2html import cleanup_xhtml
+
+from bs4 import BeautifulSoup
+
+log = logging.getLogger(__file__)
+
+
+class ServiceTheme:
+
+	PROPS = {
+		'Name'                 : None,
+	}
+
+	DEBUG = settings.DEBUG
+	BASE_DIR_FRONTEND = Path(settings.BASE_DIR_FRONTEND)
+
+	def __init__(self, id, path, url, mod):
+		self.id = id
+		self.path = path
+		self.url = url
+		self._theme_dir_name = id
+		self.script_url = None
+		self.script_crc32 = None
+
+		favicon = getattr(mod, 'favicon', None)
+
+		self.favicon_url = favicon
+		self.favicon_path = settings.BASE_DIR_FRONTEND / favicon if favicon else None
+		self.style = getattr(mod, 'style', None) or 'service.css'
+		self.script = None
+
+		if os.path.isfile(os.path.join(self.path, 'favicon.ico')):
+			self.favicon_url = self.url + '/favicon.ico'
+			self.favicon_path = self.path + '/favicon.ico'
+
+		self.ImportProps(mod)
+
+		if favicon:
+			self.favicon = FileInMemory(path=self.favicon_path, binary=True)
+			self.favicon_url += "?v={}".format(self.favicon.GetCRC())
+
+		if not self.DEBUG:
+			self.style = self.style.replace('.css', '.min.css')
+			if self.script:
+				self.script = self.script.replace('.js', '.min.js')
+
+		self.style_url = filestamp.Mark(self.style, dir=self.path, netloc=self.url, debug=self.DEBUG)
+		if self.script:
+			self.script_url = filestamp.Mark(self.script, dir=self.path, netloc=self.url, debug=self.DEBUG)
+
+		self._favicon = None
+		self._touchicons_code = ''
+		self._touchicons_files = {} # dict: '57x57' (attr "sizes") => '/static/themes/../apple-touch-icon-precomposed.png' (attr "href")
+		self.theme_script_url = ''
+		self.theme_css_url = ''
+
+		touchicons = self.path + '/touch-icons.html'
+		if os.path.isfile(touchicons):
+			with open(touchicons, 'r') as f:
+				self._touchicons_code = cleanup_xhtml(f.read(), strip_comments=True).strip().replace('\n\n', '\n')
+				soup = BeautifulSoup(self._touchicons_code, 'lxml')
+				for tag in soup.findAll('link', href=True):
+					if tag.get('rel') == 'apple-touch-icon-precomposed':
+						sizes = tag.get('sizes') or '57×57'
+						self._touchicons_files[sizes] = tag.get('href')
+
+		self.theme_css_url = filestamp.Mark(('service.css' if self.DEBUG else 'service.min.css'), dir=self.path, netloc=self.url, debug=self.DEBUG)
+
+		theme_script_url = 'script.js' if self.DEBUG else 'script.min.js'
+		if os.path.isfile(os.path.join(self.path, theme_script_url)):
+			self.theme_script_url = filestamp.Mark(theme_script_url, dir=self.path, netloc=self.url, debug=self.DEBUG)
+
+		self.OnLoadTheme(mod)
+
+	def ImportProps(self, mod):
+		for k,v in list(self.PROPS.items()):
+			setattr(self, k, getattr(mod, k, v))
+
+	def OnLoadTheme(self, mod):
+		pass
+
+	@staticmethod
+	def _crc32(filepath):
+		try:
+			with open(filepath, 'r') as f:
+				return '%x' % int(zlib.crc32(f.read()) & 0xFFFFFFFF)
+		except IOError:
+			pass
+		return None
+
+	def GetName(self):
+		return self.Name or self._theme_dir_name
+
+	def GetURL(self):
+		return self.url
+
+	def GetStyleURL(self):
+		return self.style_url
+
+	def GetScriptURL(self):
+		return self.script_url
+
+	def GetScreenshotURL(self):
+		return self.screenshot_url
+
+	def GetFaviconURL(self):
+		return self.favicon_url
+
+	def GetFaviconDir(self):
+		return self.favicon_path
+
+	def GetFavicon(self):
+		return self.favicon
+
+	def GetTouchIconsCode(self):
+		return self._touchicons_code
+
+	def GetAppleTouchIcon(self, size='180x180'):
+		return self._touchicons_files.get(size)
+
+	def GetThemeFile(self, filepath):
+		if not self.url:
+			return filepath
+		return self.url + '/' + filepath.lstrip('/')
+
+	def GetThemeCSSURL(self, request):
+		return self.theme_css_url
+
+	def GetThemeScriptURL(self, request):
+		return self.theme_script_url
+
+	
+	def __str__(self):
+		return '{}("{}")'.format(self.__class__.__name__, self.path)
+
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+
+class ServiceThemeMgrClass(object):
+
+	themes = {}
+	themes_by_url = {}
+	default = None
+	_themes_list_desktop = []
+	_themes_list_mobile = []
+
+	def GetDefault(self):
+		return self.default
+
+	def FindTheme(self, theme_id, default=True):
+		return self.themes.get(theme_id, self.default if default else None)
+
+	def FindByURL(self, theme_path, default=True):
+		return self.themes_by_url.get(theme_path, self.default if default else None)
+
+	def GetDefaultTheme(self):
+		return self.default
+
+	def Load(self, basedirs):
+
+		self._themes_list_desktop = []
+		self._themes_list_mobile = []
+		for themes_dir, themes_url in basedirs:
+
+			for theme_dir in os.listdir(themes_dir):
+				theme_path = os.path.join(themes_dir,theme_dir)
+				theme_url = themes_url+'/'+theme_dir
+				if not os.path.isdir(theme_path) or not os.path.isfile(os.path.join(theme_path, '__init__.py' )):
+					continue
+
+				mod = imp.load_source('service_theme_%s' % (theme_dir,), os.path.join(theme_path, '__init__.py' ))
+
+				theme = ServiceTheme(theme_dir, theme_path, theme_url, mod)
+				self.themes[theme_dir] = theme
+				self.themes_by_url[theme_url] = theme
+
+
+		self.default = self.themes.get('default')
+
+		self._themes_list_desktop.sort()
+		self._themes_list_mobile.sort()
+
+	def GetThemes(self, allow_limited=False):
+		collection = list(self.themes.values()) if allow_limited else [ i for i in list(self.themes.values()) if not i.is_limited ]
+		return sorted( collection, key=lambda theme: theme.name )
+
+	def GetThemesList(self, desktop=False, mobile=False):
+		if desktop:
+			return self._themes_list_desktop
+		if mobile:
+			return self._themes_list_mobile
+		return []
