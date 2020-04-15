a142ff54b781662ac98ac67ce555476230bfd584
tomaszhlawiczka <tomasz.hlawiczka@tru.pl>
2020-04-15 15:12:59 +0200

    JSON request with empty body

--- tru/dj/mw/JSONPayloadMiddleware.py
+++ tru/dj/mw/JSONPayloadMiddleware.py
@@ -1,4 +1,7 @@
 import json
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def JSONPayloadMiddleware(get_response):
@@ -9,7 +12,15 @@ def JSONPayloadMiddleware(get_response):
 		# "application/json; charset=utf-8"
 		if request.content_type.startswith('application/json'):
 
-			data = json.loads(request.body.decode("utf-8")) if request.body else None
+			data = json.loads(request.body.decode("utf-8")) if request.body else {}
+			if not isinstance(data, dict):
+				# A root object of the JSON payload should be a dict, due to django expectation
+				# for serialization of request.POST, for example here:
+				# site-packages/django/views/debug.py", line 305, in get_traceback_data
+				# 'filtered_POST_items': list(self.filter.get_post_parameters(self.request).items()),
+				# AttributeError: 'NoneType' object has no attribute 'items'
+				log.debug(f"Payload of {request.method} is not a dict: %s", data)
+
 			if request.method == 'POST':
 				request.POST = data
 			elif request.method == 'PUT':
